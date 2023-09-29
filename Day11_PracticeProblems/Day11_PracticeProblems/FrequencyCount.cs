3c452550406525cfc27f36f49a52de21a23c8986
jashulovely <porsu.jaswanthkumar@gmail.com>
2023-09-29 22:22:03 +0530

    [Jashu]Add : Elements frequency count in an Array.

---
+++ Day11_PracticeProblems/Day11_PracticeProblems/FrequencyCount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_PracticeProblems
+{
+    internal class FrequencyCount
+    {
+        public void Frequency()
+        {
+            int i, j, count, num;
+            int[] arr = new int[100];
+            int[] frequency = new int[100];
+            Console.WriteLine("Enter size of the array: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter elements in the array: ");
+            for (i = 0; i < num; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                frequency[i] = -1;
+            }
+            for (i = 0; i < num; i++)
+            {
+                count = 1;
+                for (j = i + 1; j < num; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        frequency[j] = 0;
+                    }
+                }
+                if (frequency[i] != 0)
+                {
+                    frequency[i] = count;
+                }
+            }
+            Console.WriteLine("\nFrequency of all elements of array : \n");
+            for (i = 0; i < num; i++)
+            {
+                if (frequency[i] != 0)
+                {
+                    Console.WriteLine(arr[i] + " occurs " + frequency[i] + " times");
+                }
+            }
+        }
+    }
+}
--- Day11_PracticeProblems/Day11_PracticeProblems/Program.cs
+++ Day11_PracticeProblems/Day11_PracticeProblems/Program.cs
@@ -17,7 +17,7 @@ namespace Day11_PracticeProblems
             {
                 Console.WriteLine("Enter your choice.\n   1   -->   Reverse String.\n   2   -->   Sum Of Digits in given number.\n" +
                     "   3   -->   Row matrix sum.\n   4   -->   Down Triangle Pattern.\n   5   -->   O Pattern.\n   6   -->   Duplicates Count in array.\n" +
-                    "   7   -->   Unique Elements in Array.\n   0   -->   QUIT");
+                    "   7   -->   Unique Elements in Array.\n   8   -->   Array Elements Frequency Count.\n   0   -->   QUIT");
                 int choice = int.Parse(Console.ReadLine());
                 switch (choice)
                 {
@@ -55,6 +55,11 @@ namespace Day11_PracticeProblems
                     case 7:
                         UniqueElements uniqueElements = new UniqueElements();
                         uniqueElements.Unique();
+                        Console.ReadLine();
+                        break;
+                    case 8:
+                        FrequencyCount frequencyCount = new FrequencyCount();
+                        frequencyCount.Frequency();
                         Console.ReadLine();
                         break;
                     case 0:
